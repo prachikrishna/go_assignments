1a8694245526eebcb5cf2fa720eddd14e498a68b
prachikrishna <prachikrishna20@gmail.com>
2022-02-17 15:28:35 +0530

    updated changes

--- p2.go
+++ p2.go
@@ -1,3 +1,4 @@
+//given a url,fetch all images and download them concurrently
 package main
 
 import (
@@ -17,24 +18,28 @@ var wg sync.WaitGroup
 
 func main() {
 	url := "https://golang.org"
-
+	images := findImages(url)
+	downloadImages(images)
+}
+func findImages(url string) []string {
 	result := make([]string, 0)
 	resp, err := http.Get(url)
 	// handle the error if there is one
 	if err != nil {
 		log.Fatal(err)
 	}
-	// do this now so it won't be forgotten
+
 	defer resp.Body.Close()
 	// reads html as a slice of bytes
 	html1, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		log.Fatal(err)
 	}
-	new_html := string(html1)
-	// show the HTML code as a string %s
+	new_html := string(html1) // convert slice of bytes to string
+
 	//fmt.Printf("%s\n", new_html)
 
+	//parsing
 	doc, err := html.Parse(strings.NewReader(new_html))
 	if err != nil {
 		log.Fatal(err)
@@ -55,15 +60,19 @@ func main() {
 		}
 	}
 	f(doc)
-	wg.Add(len(result))
-	for _, result := range result {
-		go func(result string) {
+	return result
+}
+func downloadImages(images []string) {
+
+	wg.Add(len(images))
+	for _, images := range images {
+		go func(images string) {
 			defer wg.Done()
-			tokens := strings.Split(result, "/")
+			tokens := strings.Split(images, "/")
 			//fmt.Println(tokens)
 
 			imageName := tokens[len(tokens)-1]
-			fmt.Println("Found", result, "as", imageName)
+			fmt.Println("Found", images, "as", imageName)
 
 			chk := tokens[0]
 			if chk == "https:" {
@@ -73,7 +82,7 @@ func main() {
 				}
 				defer output.Close()
 
-				res, err := http.Get(result)
+				res, err := http.Get(images)
 				if err != nil {
 					log.Fatal(err)
 				} else {
@@ -87,9 +96,8 @@ func main() {
 				}
 			}
 
-		}(result)
+		}(images)
 	}
 	wg.Wait()
 	fmt.Println("Done")
-
 }
